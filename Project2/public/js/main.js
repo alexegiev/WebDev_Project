653285c891acafe035494235212452619323c09c
Alexegiev Theodore <alexegievteo@gmail.com>
2023-12-06 18:24:40 +0200

    Startet experiments using handlebars

--- Project2/public/js/main.js
+++ Project2/public/js/main.js
@@ -1,27 +1,49 @@
-function countProducts(products)
-{
-    return products.length > 0 ?
-    `<p>Βρέθηκαν ${products.length} αποτελέσματα</p>` :
-    `<p>Δεν βρέθηκαν αποτελέσματα</p>`
-}
+// function countProducts(products)
+// {
+//     return products.length > 0 ?
+//     `<p>Βρέθηκαν ${products.length} αποτελέσματα</p>` :
+//     `<p>Δεν βρέθηκαν αποτελέσματα</p>`
+// }
 
-function createProduct(products)
-{
-    let content = `
-    <h1>Αποτελέσματα Αναζήτησης</h1> 
-    ${countProducts(products)}
+// function createProduct(products)
+// {
+//     let content = `
+//     <h1>Αποτελέσματα Αναζήτησης</h1> 
+//     ${countProducts(products)}
+//     <ul id="produced_list">
+//         ${products
+//             .map(function (product) {
+//                 return `<li>${product}</li>`
+//             })
+//             .join('\n')}
+//     </ul>`
+//     return content
+// }
+
+// window.addEventListener('load', function () {
+//     let products = ['Iphone', 'Samsung', 'Huawei', 'Xiaomi', 'Sony', 'LG', 'Nokia', 'Motorola', 'OnePlus', 'Google']
+//     let placeholder = document.getElementById('all_phones_list')
+//     placeholder.innerHTML = createProduct(products)
+// })
+
+
+var templates = {}
+
+templates.product = Handlebars.compile(`
+    {{#if products.length}}
+        <p>Βρέθηκαν {{products.length}} αποτελέσματα</p>
+    {{else}}
+        <p>Δεν βρέθηκαν αποτελέσματα</p>
+    {{/if}}
     <ul id="produced_list">
-        ${products
-            .map(function (product) {
-                return `<li>${product}</li>`
-            })
-            .join('\n')}
-    </ul>`
-    return content
-}
+        {{#each products}}
+            <li>{{this}}</li>
+        {{/each}}
+    </ul>
+`)
 
-window.addEventListener('load', function () {
+window.onload = function () {
     let products = ['Iphone', 'Samsung', 'Huawei', 'Xiaomi', 'Sony', 'LG', 'Nokia', 'Motorola', 'OnePlus', 'Google']
     let placeholder = document.getElementById('all_phones_list')
-    placeholder.innerHTML = createProduct(products)
-})
+    placeholder.innerHTML = templates.product({ products: products })
+}
