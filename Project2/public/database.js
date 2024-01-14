06909270e45dc28428b95f1d109666b2912b8fa6
Alexegiev Theodore <96682920+alexegiev@users.noreply.github.com>
2024-01-14 12:10:55 +0200

    Added gitognore file

--- Project2/public/database.js
+++ Project2/public/database.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb')
 
-const uri = 'mongodb+srv://Admin:rM1snTZCyjLLOTp5@cluster0.ck2m9hd.mongodb.net/?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://User:cmdGPRLl5nrPPV1D@cluster0.ck2m9hd.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
